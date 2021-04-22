273b3c0d0f1fba866df22ecac241d35475592e62
saneenKP <saneenkp2000@gmail.com>
2021-04-22 19:36:27 +0530

    place Order UI completed

--- app/src/main/java/com/example/temp/PlaceOrder.java
+++ app/src/main/java/com/example/temp/PlaceOrder.java
@@ -3,10 +3,16 @@ package com.example.temp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PlaceOrder extends AppCompatActivity {
 
+    private TextView payableAmount;
+    private TextView location;
+    private TextView Add_houseNo , Add_locality , Add_landmark , Add_district;
+
+    private Button pay_and_order , cash_on_delivery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,18 @@ public class PlaceOrder extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         int totalAmount = b.getInt("totalAmount");
 
+        pay_and_order = findViewById(R.id.PAD);
+        cash_on_delivery = findViewById(R.id.COD);
+
+        payableAmount = findViewById(R.id.payAmount);
+        location = findViewById(R.id.location);
+        Add_houseNo = findViewById(R.id.Add_houseNo);
+        Add_locality = findViewById(R.id.Add_locality);
+        Add_landmark = findViewById(R.id.Add_landmark);
+        Add_district = findViewById(R.id.Add_district);
+
+
+        payableAmount.setText(Integer.toString(totalAmount) + " \u20B9");
 
 
 
