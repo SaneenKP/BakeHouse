cedf32f3b2ab79d894a311c7071c75063dd96bcb
saneenKP <saneenkp2000@gmail.com>
2021-04-23 17:32:43 +0530

    location functionality and methods completed

--- app/src/main/java/com/example/temp/PlaceOrder.java
+++ app/src/main/java/com/example/temp/PlaceOrder.java
@@ -1,18 +1,39 @@
 package com.example.temp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class PlaceOrder extends AppCompatActivity {
 
     private TextView payableAmount;
-    private TextView location;
+    private TextView longitude , lattitude;
     private TextView Add_houseNo , Add_locality , Add_landmark , Add_district;
-
+    private FusedLocationProviderClient fusedLocationProviderClient;
     private Button pay_and_order , cash_on_delivery;
+    private int PERMISSION_ID = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +47,118 @@ public class PlaceOrder extends AppCompatActivity {
         cash_on_delivery = findViewById(R.id.COD);
 
         payableAmount = findViewById(R.id.payAmount);
-        location = findViewById(R.id.location);
+        longitude = findViewById(R.id.longitude);
+        lattitude = findViewById(R.id.lattitude);
         Add_houseNo = findViewById(R.id.Add_houseNo);
         Add_locality = findViewById(R.id.Add_locality);
         Add_landmark = findViewById(R.id.Add_landmark);
         Add_district = findViewById(R.id.Add_district);
 
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         payableAmount.setText(Integer.toString(totalAmount) + " \u20B9");
 
-
-
+        getLastLocation();
 
     }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation()
+    {
+        if (checkPermissions())
+        {
+            if (checkLocation())
+            {
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+
+                        Location location = task.getResult();
+                        if (location == null)
+                        {
+                            RequestLocationData();
+                        }
+                        else
+                        {
+                            lattitude.setText(location.getLatitude()+" ");
+                            longitude.setText(location.getLongitude()+" ");
+
+                        }
+                    }
+                });
+
+            }
+            else
+            {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        }
+        else {
+            requestPermissions();
+        }
+
+    }
+    @SuppressLint("MissingPermission")
+    private void RequestLocationData()
+    {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5);
+        locationRequest.setFastestInterval(0);
+        locationRequest.setNumUpdates(1);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest , locationCallback , Looper.myLooper());
+    }
+
+    private LocationCallback locationCallback = new LocationCallback(){
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+           Location location = locationResult.getLastLocation();
+           lattitude.setText("Latitude = " + location.getLatitude() + " ");
+           longitude.setText("Longitutde" + location.getLongitude()+ " ");
+        }
+    };
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    private boolean checkLocation()
+    {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+
+
 }
