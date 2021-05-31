e3cfc22c9c89c31219100eedf16e1296e7f909b3
saneenKP <saneenkp2000@gmail.com>
2021-06-01 02:52:12 +0530

    add hotel implementation over with firebase storage circular progress bar visiblity left

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,1 +1,2 @@
 -keepclassmembers class * {    @android.webkit.JavascriptInterface <methods>;}-keepattributes JavascriptInterface-keepattributes *Annotation*-dontwarn com.razorpay.**-keep class com.razorpay.** {*;}-optimizations !method/inlining/*-keepclasseswithmembers class * {  public void onPayment*(...);}
+-keep class androidx.appcompat.widget.** { *; }
--- app/src/main/java/com/example/temp/Hotels.java
+++ app/src/main/java/com/example/temp/Hotels.java
@@ -1,13 +1,19 @@
-package com.example.temp;
+ package com.example.temp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -16,15 +22,28 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Hotels extends AppCompatActivity {
 
@@ -34,6 +53,10 @@ public class Hotels extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private List<String> hotelKeys;
     private FloatingActionButton fab;
+    private Boolean imageFlag = false;
+    private  Uri resultUri;
+    private AlertDialog dialog;
+    private CircularProgressIndicator circularProgressIndicator;
 
 
     @Override
@@ -43,6 +66,7 @@ public class Hotels extends AppCompatActivity {
 
         hotels = findViewById(R.id.hotels);
         layoutManager =  new LinearLayoutManager(this);
+        circularProgressIndicator = findViewById(R.id.circularProgress);
 
         hotelsList = new ArrayList<>();
         hotelKeys = new ArrayList<>();
@@ -84,7 +108,79 @@ public class Hotels extends AppCompatActivity {
 
     }
 
-    private void addNewHotel(){
+    private void addNewHotel(HotelDetails hotelDetails){
+
+        if (resultUri == null){
+            Toast.makeText(getApplicationContext() , "Please Select An Image" , Toast.LENGTH_LONG).show();
+        }
+        else{
+
+            StorageReference hotelImage = FirebaseStorage.getInstance().getReference().child(getApplicationContext().getString(R.string.HotelNode)+"/"+hotelDetails.getHotel_name());
+            hotelImage.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                    if (task.isSuccessful()){
+
+                        hotelImage.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                if (task.isSuccessful()){
+
+                                    hotelDetails.setImage(task.getResult().toString());
+                                    databaseReference.push().setValue(hotelDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+
+                                                circularProgressIndicator.setProgress(0);
+                                                circularProgressIndicator.setVisibility(View.INVISIBLE);
+                                                resultUri = null;
+                                                dialog.dismiss();
+
+                                            }else{
+                                                Toast.makeText(getApplicationContext() , "Network Error" + Objects.requireNonNull(task.getException().getMessage()) , Toast.LENGTH_LONG).show();
+                                                circularProgressIndicator.setVisibility(View.INVISIBLE);
+                                            }
+                                        }
+                                    });
+
+                                }else
+                                {
+                                    Toast.makeText(getApplicationContext() , "Network Error" + Objects.requireNonNull(task.getException().getMessage()) , Toast.LENGTH_LONG).show();
+                                    circularProgressIndicator.setProgress(0);
+                                    circularProgressIndicator.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        });
+
+
+                    }else{
+                        Toast.makeText(getApplicationContext() , "Network Error" + Objects.requireNonNull(task.getException().getMessage()) , Toast.LENGTH_LONG).show();
+                        circularProgressIndicator.setProgress(0);
+                        circularProgressIndicator.setVisibility(View.INVISIBLE);
+
+                    }
+
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+
+                    circularProgressIndicator.setVisibility(View.VISIBLE);
+                    double progress = (100*snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                    circularProgressIndicator.setProgressCompat((int)progress,true);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Toast.makeText(getApplicationContext() , ""+e.getMessage() , Toast.LENGTH_LONG).show();
+                    circularProgressIndicator.setVisibility(View.INVISIBLE);
+                }
+            });
+        }
 
     }
 
@@ -99,17 +195,56 @@ public class Hotels extends AppCompatActivity {
         EditText hotelAddress = v.findViewById(R.id.newHotelAddress);
         EditText hotelLocation = v.findViewById(R.id.newHotelLocation);
         Button addHotel = v.findViewById(R.id.addNewHotel);
+        Button addImage = v.findViewById(R.id.addNewHotelImage);
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               CropImage.activity().setGuidelines(CropImageView.Guidelines.ON)
+                       .setAspectRatio(1, 1)
+                       .start(Hotels.this);
+
+            }
+        });
+
 
         addHotel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
+                if (TextUtils.isEmpty(hotelName.getText())){
+                    hotelName.setError("Please set Hotel Name");
+                }else if (TextUtils.isEmpty(hotelAddress.getText())){
+                    hotelAddress.setError("Please set Address");
+                }else if(TextUtils.isEmpty(hotelLocation.getText())){
+                    hotelLocation.setError("Please give the location");
+                }else{
+                    HotelDetails hotelDetails = new HotelDetails();
+                    hotelDetails.setHotel_name(hotelName.getText().toString());
+                    hotelDetails.setAddress(hotelAddress.getText().toString());
+                    hotelDetails.setLocation(hotelLocation.getText().toString());
+                    addNewHotel(hotelDetails);
+                }
+
             }
         });
 
-        AlertDialog dialog = builder.create();
+        dialog = builder.create();
         dialog.setCanceledOnTouchOutside(true);
         dialog.show();
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+           if (resultCode == RESULT_OK){
+               resultUri = Objects.requireNonNull(result).getUri();
+        }
+    }
+
 }
