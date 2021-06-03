670ad62c3951e182dda43d7b8bd5f6faf8f7b657
saneenKP <saneenkp2000@gmail.com>
2021-06-04 01:03:46 +0530

    Services alert dialog box implemented with floating action button

--- app/src/main/java/com/example/temp/Services.java
+++ app/src/main/java/com/example/temp/Services.java
@@ -1,22 +1,33 @@
 package com.example.temp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Services extends AppCompatActivity {
 
@@ -24,11 +35,18 @@ public class Services extends AppCompatActivity {
     private List<ServiceDetails> servicesName;
     private DatabaseReference getServicesReference;
     private List<String> serviceKeys;
+    private FloatingActionButton fab;
+    private CircularProgressIndicator circularProgressIndicator;
+    private AlertDialog dialog;
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+
+        fab = findViewById(R.id.addNewService);
+        circularProgressIndicator = findViewById(R.id.circularProgress);
 
         services = findViewById(R.id.services);
         servicesName = new ArrayList<>();
@@ -62,6 +80,93 @@ public class Services extends AppCompatActivity {
                 startActivity(getVendor);
             }
         });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    private void showAlertDialog(ServiceDetails serviceDetails , String key , boolean updateStatus){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v = getLayoutInflater().inflate(R.layout.add_dish_alertdialog , null);
+
+        builder.setView(v);
+        builder.setMessage(" Add New Service ");
+
+        EditText serviceName = v.findViewById(R.id.newServiceName);
+        Button addService = v.findViewById(R.id.addNewService);
+        Button addImage = v.findViewById(R.id.addNewServiceImage);
+        Button delete = v.findViewById(R.id.deleteService);
+        delete.setVisibility(View.INVISIBLE);
+
+
+        if (updateStatus){
+
+            delete.setVisibility(View.VISIBLE);
+            serviceName.setText(serviceDetails.getName());
+        }
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                deleteDish(dishDetails.getPic() , key);
+
+            }
+        });
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1, 1)
+                        .start(Services.this);
+
+            }
+        });
+
+        addService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(serviceName.getText())){
+                    serviceName.setError("Please set Dish Name");
+                }else{
+                    ServiceDetails newServiceDetails = new ServiceDetails();
+                    newServiceDetails.setName(serviceName.getText().toString());
+
+                    if(updateStatus){
+                        newServiceDetails.setCover_pic(serviceDetails.getCover_pic());
+                        //updateDish(newDishDetails , key);
+                    }else{
+                        //addNewDish(newDishDetails,key);
+                    }
+
+                }
+
+            }
+        });
+
+        dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+        if (resultCode == RESULT_OK){
+            resultUri = Objects.requireNonNull(result).getUri();
+        }
+    }
+
 
 }
