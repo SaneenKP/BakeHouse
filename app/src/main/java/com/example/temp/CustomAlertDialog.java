2cea8c2a85bbe777feff9424ffdd73809e22f7c7
saneenKP <saneenkp2000@gmail.com>
2021-06-06 01:20:08 +0530

    Loading dialog added

--- app/src/main/java/com/example/temp/CustomAlertDialog.java
+++ app/src/main/java/com/example/temp/CustomAlertDialog.java
@@ -3,45 +3,28 @@ package com.example.temp;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 
 public class CustomAlertDialog {
 
+    private androidx.appcompat.app.AlertDialog.Builder builder;
+    private androidx.appcompat.app.AlertDialog dialog;
     private Context context;
-    private String message;
-    private int layout;
-    private boolean SetCancellationOnTouchOutside;
 
+    public CustomAlertDialog(Context context) {
 
-
-    public CustomAlertDialog(Context context, String message, int layout, boolean setCancellationOnTouchOutside) {
         this.context = context;
-        this.message = message;
-        this.layout = layout;
-        SetCancellationOnTouchOutside = setCancellationOnTouchOutside;
     }
 
-    private AlertDialog showAlertDialog(){
+    public AlertDialog getDialog(){
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = inflater.inflate(layout , null);
-        builder.setView(v);
-        builder.setMessage(message);
-
-        EditText hotelName = v.findViewById(R.id.newHotelName);
-        EditText hotelAddress = v.findViewById(R.id.newHotelAddress);
-        EditText hotelLocation = v.findViewById(R.id.newHotelLocation);
-        Button addHotel = v.findViewById(R.id.addNewHotel);
-
-        AlertDialog dialog = builder.create();
-        dialog.setCanceledOnTouchOutside(SetCancellationOnTouchOutside);
+        builder = new androidx.appcompat.app.AlertDialog.Builder(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+        View view = inflater.inflate(R.layout.custom_loading_dialog, null);
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
         return dialog;
-
     }
-
 }
