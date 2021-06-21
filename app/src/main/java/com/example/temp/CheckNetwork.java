94371421d1ba1bc79e88841399a67c0cc67a0451
saneenKP <saneenkp2000@gmail.com>
2021-06-22 00:32:53 +0530

    modified check network class with more variables to make the code cleaner in activity classes

--- app/src/main/java/com/example/temp/CheckNetwork.java
+++ app/src/main/java/com/example/temp/CheckNetwork.java
@@ -2,21 +2,70 @@ package com.example.temp;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class CheckNetwork {
 
     private Context context;
+    private ViewGroup group;
+    private CustomAlertDialog customAlertDialog;
+    private AlertDialog alertDialog;
+    private Snackbar snackbar;
 
-    public CheckNetwork(Context context) {
+
+    public CheckNetwork(Context context, ViewGroup group) {
         this.context = context;
+        this.group = group;
+        customAlertDialog = new CustomAlertDialog(context , "Loading");
     }
 
-    public boolean isNetworkConnected() {
+    public boolean check(){
+
+        boolean status = false;
+
+        alertDialog = customAlertDialog.showAlertDialog();
+        if (isNetworkConnected()){
+
+            status = true;
+            if (internetIsConnected()){
+
+                alertDialog.dismiss();
+                status = true;
+            }else{
+
+                alertDialog.dismiss();
+                status = false;
+                if (group!=null){
+                    snackbar.make(group , getNoNetworkConnectionError() , Snackbar.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(context, getNoNetworkConnectionError(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }else{
+            alertDialog.dismiss();
+            status = false;
+            if (group!=null){
+                snackbar.make(group , getInternetNotSwitchedOnError() , Snackbar.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(context, getInternetNotSwitchedOnError(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        return status;
+    }
+
+    private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null;
     }
 
-    public boolean internetIsConnected() {
+    private boolean internetIsConnected() {
         try {
             String command = "ping -c 1 google.com";
             return (Runtime.getRuntime().exec(command).waitFor() == 0);
@@ -25,10 +74,10 @@ public class CheckNetwork {
         }
     }
 
-    public String getInternetNotSwitchedOnError(){
+    private String getInternetNotSwitchedOnError(){
         return context.getResources().getString(R.string.networkNotSwitchedOn);
     }
-    public String getNoNetworkConnectionError(){
+    private String getNoNetworkConnectionError(){
         return context.getResources().getString(R.string.noInternet);
     }
 
